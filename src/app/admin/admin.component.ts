4c77a8011db590bdb68d7bb35ccbca89c0f84edf
Nebiyou <nebHailemariam@gmail.com>
2021-05-26 13:23:50 +0300

    Initialization

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../model/user';
+import { UserService } from '../services/user/user.service';
+
+@Component({ templateUrl: 'admin.component.html' })
+export class AdminComponent implements OnInit {
+  loading = false;
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.loading = true;
+    this.userService
+      .getAll()
+      .pipe(first())
+      .subscribe((users: User[]) => {
+        this.loading = false;
+        this.users = users;
+      });
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../model/user';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { UserService } from '../services/user/user.service';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+  loading = false;
+  user: User;
+  userFromApi: User;
+
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.user = this.authenticationService.userValue;
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    this.userService
+      .getById(this.user.id)
+      .pipe(first())
+      .subscribe((user: User) => {
+        this.loading = false;
+        this.userFromApi = user;
+      });
+  }
+}
